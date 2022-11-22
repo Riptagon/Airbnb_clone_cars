6ca4508da6e18336aa1893780b69eefb338e9356
Casimir von Meyer zu Knonow <casimir@meyer-knonow.de>
2022-11-22 15:59:03 +0100

    new create pictue upload

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -8,14 +8,25 @@ class CarsController < ApplicationController
   end
 
   def new
-
+    @car = Car.new
   end
 
   def create
-
+    @car = Car.new(cars_params)
+    if @car.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
 
+  end
+
+  private
+
+  def cars_params
+    params.require(:car).permit(:brand, :model, :color, :kilometers, :year, :fuel_consumption, :price, :hp, :photo)
   end
 end
