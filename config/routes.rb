de9f2240e11ef2a604590ae3cf4f415df58ca905
Bilhid Acheampong <bil.bilhid09@gmail.com>
2022-11-23 12:57:20 +0100

    booking logic

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :cars, except: %i[index edit update]
+  resources :cars, except: %i[index edit update] do
+    resources :bookings, only: %i[new create]
+  end
 end
