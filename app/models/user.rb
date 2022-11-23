9c4e72a77911b1db8335016174684047b3797801
Bilhid Acheampong <bil.bilhid09@gmail.com>
2022-11-23 11:51:40 +0100

    updated all models and added the property model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_many :bookings
   has_many :cars, through: :bookings
-  
+  has_many :property
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_132015) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_104000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_132015) do
     t.integer "hp"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+  end
+
+  create_table "properties", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "car_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_properties_on_car_id"
+    t.index ["user_id"], name: "index_properties_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -81,4 +91,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_132015) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "cars"
   add_foreign_key "bookings", "users"
+  add_foreign_key "properties", "cars"
+  add_foreign_key "properties", "users"
 end
