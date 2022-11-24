8fe49d306be54383083efcf2029d0f6910bec456
Bilhid Acheampong <bil.bilhid09@gmail.com>
2022-11-24 17:25:39 +0100

    image upload for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :bookings
   has_many :cars
+  has_one_attached :photo
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   validates_presence_of :username, :email, :password
